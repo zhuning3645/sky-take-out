95a352e05347fc7f2c171a7fe3d03175ddad2196
fero <877954032@qq.com>
2024-07-01 10:51:31 +0800

    菜品销售统计

--- sky-server/src/main/java/com/sky/service/ReportService.java
+++ sky-server/src/main/java/com/sky/service/ReportService.java
@@ -1,6 +1,7 @@
 package com.sky.service;
 
 import com.sky.vo.OrderReportVO;
+import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 
@@ -24,5 +25,13 @@ public interface ReportService {
      *统计指定时间区间内的订单数据
      * @return
      */
-    OrderReportVO getOrderStatistics(LocalDate begin, LocalDate end);
+   OrderReportVO getOrderStatistics(LocalDate begin, LocalDate end);
+
+    /**
+     * 统计指定时间区间内的销量排名前10
+     * @param begin
+     * @param end
+     * @return
+     */
+    SalesTop10ReportVO getSalesTop10(LocalDate begin, LocalDate end);
 }
--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -1,10 +1,12 @@
 package com.sky.service.impl;
 
+import com.sky.dto.GoodsSalesDTO;
 import com.sky.entity.Orders;
 import com.sky.mapper.OrderMapper;
 import com.sky.mapper.UserMapper;
 import com.sky.service.ReportService;
 import com.sky.vo.OrderReportVO;
+import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 import lombok.extern.slf4j.Slf4j;
@@ -21,6 +23,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -149,14 +152,12 @@ public class ReportServiceImpl implements ReportService {
             //查询每天的有效订单数select count(id) from orders where order_time >? and order_time <? and status = 5
             Integer validOrderCount = getOrderCount(beginTime,endTime,Orders.COMPLETED);
             validOrderCountList.add(validOrderCount);
-            log.info("时间：{},订单数：{}",beginTime,validOrderCount);
 
         }
         //计算时间区间内的订单总数量
         Integer totalOrderCount = orderCountList.stream().reduce(Integer::sum).get();
         //计算时间区间内的有效订单总数量
         Integer validOrderCount = validOrderCountList.stream().reduce(Integer::sum).get();
-        log.info("有效订单数：{}", validOrderCount);
 
         Double orderCompletionRate = 0.0;
         if (totalOrderCount != 0) {
@@ -187,5 +188,30 @@ public class ReportServiceImpl implements ReportService {
         map.put("status", status);
 
         return orderMapper.countByMap(map);
+    }
+
+    /**
+     * 统计指定时间区间内的销量排名前10
+     * @param begin
+     * @param end
+     * @return
+     */
+    public SalesTop10ReportVO getSalesTop10(LocalDate begin, LocalDate end) {
+        LocalDateTime beginTime = LocalDateTime.of(begin, LocalTime.MIN);
+        LocalDateTime endTime = LocalDateTime.of(end, LocalTime.MAX);
+
+        List<GoodsSalesDTO> salesTop10 = orderMapper.getSalesTop10(beginTime, endTime);
+        List<String>names = salesTop10.stream().map(GoodsSalesDTO::getName).collect(Collectors.toList());
+        String nameList = StringUtils.join(names, ",");
+        log.info("查询到的菜品名称：{}",nameList);
+
+        List<Integer>numbers = salesTop10.stream().map(GoodsSalesDTO::getNumber).collect(Collectors.toList());
+        String numberList = StringUtils.join(numbers,",");
+
+        //封装返回结果数据
+        return SalesTop10ReportVO.builder()
+                .nameList(nameList)
+                .numberList(numberList)
+                .build();
     }
 }
